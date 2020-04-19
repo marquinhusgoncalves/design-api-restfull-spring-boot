4ada2feeb845276b1036bf860773f86516b2997e
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-04-18 22:23:34 -0300

    chore: Criando nosso primeiro teste de integração com a base de dados

---
+++ library-api/src/test/java/com/marquinhus/libraryapi/model/repository/BookRespositoryTest.java
@@ -0,0 +1,37 @@
+package com.marquinhus.libraryapi.model.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest
+public class BookRespositoryTest {
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    BookRepository repository;
+
+    @Test
+    @DisplayName("Deve retornar verdadeiro quando existir um livro na base com o isbn informado")
+    public void returnTrueWhenisbnExists() {
+        // cenario
+        String isbn = "123";
+
+        // execucao
+        Boolean exists = repository.existsByIsbn(isbn);
+
+        // verificação
+        assertThat(exists).isTrue();
+    }
+}
